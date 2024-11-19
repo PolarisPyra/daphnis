f74e93a468080c1875b59eb399ddb2ab1561cae1
Polaris <mail@example.com>
2024-11-19 00:09:54 -0500

    asc to desc

--- components/allSongs/actions.ts
+++ components/allSongs/actions.ts
@@ -23,7 +23,7 @@ export async function getChunithmSongList() {
         genre: true,
         jacketPath: true,
       },
-      orderBy: [{ songId: "asc" }, { chartId: "desc" }],
+      orderBy: [{ songId: "desc" }, { chartId: "desc" }],
     });
   } catch (error) {
     console.error("Error fetching song list:", error);
